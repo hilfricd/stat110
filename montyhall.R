d90cf1c6b5aa2a4de834de1c10d9324d861233ac
David Hilfrich <dhilfrich@gmail.com>
2023-12-29 13:30:20 +0100

    Added alwaysswitching strategy

--- montyhall.R
+++ montyhall.R
@@ -1,9 +1,31 @@
 
 ## Simulates the Monty-Hall-Cardoor-Problem n times
-simulate_monty_hall <- function(n = 1000, switch = TRUE) {
+simulate_monty_hall <- function(n = 1000, switch = TRUE, n_doors = 3) {
+  doors <- 1:n_doors
+  # randomly pick car door for each n
+  cardoors <- sample(doors, n, replace = TRUE)
+
   if(!switch) {
-    cardoor <- sample(3, n, replace = TRUE)
-    wins <- sum(cardoor == 1)
+    # if we don't switch we assume we always picked door 1
+    wins <- sum(cardoors == 1)
+  }
+  else {
+    # if we always switch we select the doors that monty opened,
+    # then switch to the remaining door,
+    # and count the number of times we won using that strategy
+    wins <- 0
+    for(cardoor in cardoors) {
+      # possible doors for Monty
+      montydoor <- doors[-c(1, cardoor)]
+
+      # if more than 1, choose one at random
+      if(length(montydoor) > 1) {
+        montydoor <- sample(montydoor, 1)
+      }
+      # switch to remaining door
+      chosendoor <- doors[-c(1, montydoor)]
+      if(chosendoor == cardoor) wins <- wins+1
+    }
   }
 
   print(paste0("Monty Hall Problem simulated ", n, " times!"))
