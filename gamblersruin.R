9aee5cb027557f7a1c82ff84f836be7ccb477964
David Hilfrich <dhilfrich@gmail.com>
2023-12-30 09:17:03 +0100

    Simulation of one round of gambler's ruin

--- gamblersruin.R
+++ gamblersruin.R
@@ -1,1 +1,14 @@
  # simulate one round of gambler's ruin
+
+i <- 10
+N <- 20
+p_a <- 0.5
+
+n_rounds <- 0
+while(i > 0 & i < N) {
+  # next bet
+  result <- sample(c(1, -1), 1, prob = c(p_a, 1-p_a))
+
+  i <- i + result
+  n_rounds <- n_rounds + 1
+}
