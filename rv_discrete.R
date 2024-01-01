977466da30d9aef48b90687aa533acecba4fe723
David Hilfrich <dhilfrich@gmail.com>
2024-01-01 20:06:56 +0100

    Bugfix

--- rv_discrete.R
+++ rv_discrete.R
@@ -1,20 +1,21 @@
 library(dplyr)
+library(tidyr)
 library(ggplot2)
 
 #################################
 #       BINOMIAL                #
 #################################
 
-trials <- 10
-p <- 0.5
-
-
 plot_binom_pmf <- function(trials, p) {
   rv_bin <- tibble(
-    k = rbinom(10000, n, p)) %>%
-    count(k) %>%
-    mutate(frac = n/sum(n)) %>%
-    mutate(pmf = dbinom(0:trials, trials, p))
+    k = 0:trials,
+    pmf = dbinom(0:trials, trials, p)) %>%
+    left_join(
+      tibble(k = rbinom(10000, trials, p)) %>% count(k), by = "k") %>%
+    mutate(
+      n = replace_na(n, 0),
+      frac = n/sum(n)
+    )
 
   ggplot(rv_bin, aes(k, frac)) +
     geom_col(fill = "skyblue", alpha = 0.8) +
@@ -26,3 +27,4 @@ plot_binom_pmf <- function(trials, p) {
          subtitle = "Randomly sampled distribution (n=10,000, light blue),\nand corresponding PMF (dark blue)",
          y = "Relative frequency / probability")
 }
+plot_binom_pmf(20, 0.3)
