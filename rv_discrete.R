7b09ead686e5fae0b9a1ba6406a294206149a3dd
David Hilfrich <dhilfrich@gmail.com>
2024-01-02 12:52:03 +0100

    Added hypergeometric distribution

--- rv_discrete.R
+++ rv_discrete.R
@@ -27,4 +27,30 @@ plot_binom_pmf <- function(trials, p) {
          subtitle = "Randomly sampled distribution (n=10,000, light blue),\nand corresponding PMF (dark blue)",
          y = "Relative frequency / probability")
 }
-plot_binom_pmf(20, 0.3)
+
+
+#################################
+#       HYPERGEOMETRIC          #
+#################################
+plot_hypergeom_pmf <- function(w, b, draws) {
+  rv_hyper <- tibble(
+    k = 0:draws,
+    pmf = dhyper(0:draws, w, b, draws)) %>%
+    left_join(
+      tibble(k = rhyper(10000, w, b, draws)) %>% count(k), by = "k") %>%
+    mutate(
+      n = replace_na(n, 0),
+      frac = n/sum(n)
+    )
+
+  ggplot(rv_hyper, aes(k, frac)) +
+    geom_col(fill = "skyblue", alpha = 0.8) +
+    geom_col(aes(y = pmf),
+             color = "darkblue", fill = NA, alpha = 0.8) +
+    scale_x_continuous(breaks = 0:draws) +
+    theme_classic() +
+    labs(title = paste0("HGeom(", w, ", ", b, ", ", draws, ")"),
+         subtitle = "Randomly sampled distribution (n=10,000, light blue),\nand corresponding PMF (dark blue)",
+         x = "White marbles",
+         y = "Relative frequency / probability")
+}
